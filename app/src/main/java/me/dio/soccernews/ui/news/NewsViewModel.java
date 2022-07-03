dd65ec3d39125b5b63fcd97797ce678bd11cc7bf
Kaleniuk <kaleniuk7@yahoo.com.br>
2022-07-03 00:03:11 -0300

    Inclusão da API e Retrofit

--- app/src/main/java/me/dio/soccernews/ui/news/NewsViewModel.java
+++ app/src/main/java/me/dio/soccernews/ui/news/NewsViewModel.java
@@ -1,28 +1,67 @@
 package me.dio.soccernews.ui.news;
 
+import android.util.Log;
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import me.dio.soccernews.data.remote.SoccerNewsApi;
 import me.dio.soccernews.domain.News;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class NewsViewModel extends ViewModel {
 
-    private final MutableLiveData<List<News>> news;
+    private final MutableLiveData<List<News>> news = new MutableLiveData<>();
+    private final SoccerNewsApi api;
 
     public NewsViewModel() {
-        this.news = new MutableLiveData<>();
+//       this.news = new MutableLiveData<>();
 
-        List<News> news = new ArrayList<>();
-        news.add(new News("Ferroviaria tem desfalque "," is simply dummy text of the printing and typesetting" ));
-        news.add(new News("Ferroviaria tem desfalque "," It is a long established fact that a reader will be distracted by the" ));
-        news.add(new News("Ferroviaria tem desfalque "," is simply dummy text of the printing and typesetting" ));
-        news.add(new News("Ferroviaria tem desfalque "," It is a long established fact that a reader will be distracted by the" ));
+//        List<News> news = new ArrayList<>();
+//        news.add(new News("Ferroviaria tem desfalque "," is simply dummy text of the printing and typesetting" ));
+//        news.add(new News("Ferroviaria tem desfalque "," It is a long established fact that a reader will be distracted by the" ));
+//        news.add(new News("Ferroviaria tem desfalque "," is simply dummy text of the printing and typesetting" ));
+//        news.add(new News("Ferroviaria tem desfalque "," It is a long established fact that a reader will be distracted by the" ));
+//       this.news.setValue(news);
 
-        this.news.setValue(news);
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://kaleniuk.github.io/soccer-news-api/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        api = retrofit.create(SoccerNewsApi.class);
+        this.findNews();
+    }
+
+
+    private void findNews() {
+        api.getNews().enqueue(new Callback<List<News>>() {
+            @Override
+            public void onResponse(Call<List<News>> call, Response<List<News>> response) {
+
+                if (response.isSuccessful()){
+                    news.setValue(response.body());
+                }else{
+
+
+                    //TODO Tratamento de erros
+                }
+
+            }
+
+            @Override
+            public void onFailure(Call<List<News>> call, Throwable t) {
+                     //TODO Tratamento de erros
+                Log.e("erro api", "Não deu sucesso ");
+
+            }
+        });
     }
 
     public LiveData<List<News>> getNews() {
