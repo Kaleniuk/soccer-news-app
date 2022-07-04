08910d85bf701a36daaccb58a0e05922d9e3d5f0
Kaleniuk <kaleniuk7@yahoo.com.br>
2022-07-04 20:11:52 -0300

    Inclusão da Abertura do Link da Notícia

--- app/src/main/java/me/dio/soccernews/ui/adapter/NewsAdapter.java
+++ app/src/main/java/me/dio/soccernews/ui/adapter/NewsAdapter.java
@@ -1,5 +1,7 @@
 package me.dio.soccernews.ui.adapter;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
@@ -38,6 +40,16 @@ public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.ViewHolder> {
         Picasso.get().load(news.getImage())
                 .fit()
                 .into(holder.binding.ivThumbnail);
+
+        holder.binding.btOpenLink.setOnClickListener(view ->      // botão de abrir o link
+        {
+
+            Intent i = new Intent(Intent.ACTION_VIEW);
+            i.setData(Uri.parse(news.getLink()));
+            holder.itemView.getContext().startActivity(i);
+
+        });
+
     }
 
     @Override
